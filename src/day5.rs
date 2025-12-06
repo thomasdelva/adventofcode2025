2d2ed11374912f9620702c8897da6a52dcd0b22c
thomasdelva <thomdelva@gmail.com>
2025-12-06 09:04:53 +0100

    solve day 5 without rangeset

--- src/day5.rs
+++ src/day5.rs
@@ -1,7 +1,4 @@
-use std::ops::RangeInclusive;
-
 use aoc_runner_derive::{aoc, aoc_generator};
-use range_set::RangeSet;
 
 #[aoc_generator(day5)]
 fn parse_input(input: &str) -> (Vec<(u64, u64)>, Vec<u64>) {
@@ -28,20 +25,60 @@ fn parse_input(input: &str) -> (Vec<(u64, u64)>, Vec<u64>) {
 
 #[aoc(day5, part1)]
 fn part1((ranges, numbers): &(Vec<(u64, u64)>, Vec<u64>)) -> usize {
-    let mut range_set: RangeSet<[RangeInclusive<u64>; 8]> = RangeSet::new();
-    for &(start, end) in ranges {
-        range_set.insert_range(start..=end);
+    let sorted_disjoint_ranges = sorted_disjoint_ranges(ranges);
+
+    let mut sorted_numbers = numbers.clone();
+    sorted_numbers.sort();
+
+    let mut count = 0;
+
+    // Iterate over both the sorted ranges and the sorted numbers at the same time.
+    let mut rangerator = sorted_disjoint_ranges.iter().peekable();
+    let mut numberator = sorted_numbers.iter().peekable();
+    while let (Some((start, end)), Some(&number)) = (rangerator.peek(), numberator.peek()) {
+        // Advance either the number or range iterator, whichever is behind.
+        if number < start {
+            numberator.next();
+        } else if number > end {
+            rangerator.next();
+        } else {
+            count += 1;
+            numberator.next();
+        }
     }
 
-    numbers.iter().filter(|&n| range_set.contains(*n)).count()
+    count
 }
 
 #[aoc(day5, part2)]
-fn part2((ranges, _): &(Vec<(u64, u64)>, Vec<u64>)) -> usize {
-    let mut range_set: RangeSet<[RangeInclusive<u64>; 8]> = RangeSet::new();
-    for &(start, end) in ranges {
-        range_set.insert_range(start..=end);
+fn part2((ranges, _): &(Vec<(u64, u64)>, Vec<u64>)) -> u64 {
+    sorted_disjoint_ranges(ranges)
+        .iter()
+        .map(|&(start, end)| end - start + 1)
+        .sum()
+}
+
+fn sorted_disjoint_ranges(ranges: &Vec<(u64, u64)>) -> Vec<(u64, u64)> {
+    let mut sorted_ranges = ranges.clone();
+    sorted_ranges.sort_by_key(|&(start, _)| start);
+
+    let mut sorted_disjoint_ranges = Vec::new();
+    let mut active_range: Option<(u64, u64)> = None;
+    for (start, end) in sorted_ranges {
+        match active_range {
+            Some((active_start, active_end)) if start > active_end + 1 => {
+                sorted_disjoint_ranges.push((active_start, active_end));
+                active_range = Some((start, end));
+            }
+            Some((active_start, active_end)) => {
+                active_range = Some((active_start, u64::max(active_end, end)));
+            }
+            None => active_range = Some((start, end)),
+        }
     }
 
-    range_set.len()
+    if let Some((start, end)) = active_range {
+        sorted_disjoint_ranges.push((start, end));
+    }
+    sorted_disjoint_ranges
 }
